1322d30ebbe2b2de142147b14f11720a9d088a7f
kuniaki <k.ishikawa.0128@gmail.com>
2020-06-13 15:49:00 +0900

    introduce RecyclerView

---
+++ app/src/main/java/com/fithub/singlefragmentactivity/ListAdapter.kt
@@ -0,0 +1,21 @@
+package com.fithub.singlefragmentactivity
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter(private val list: List<Movie>)
+    : RecyclerView.Adapter<MovieViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return MovieViewHolder(inflater, parent)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = list[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount() = list.size
+}
--- app/src/main/java/com/fithub/singlefragmentactivity/MainFragment.kt
+++ app/src/main/java/com/fithub/singlefragmentactivity/MainFragment.kt
@@ -5,8 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
+
+    private val mNicolasCageMovies = listOf(
+        Movie("Raising Arizona", 1987),
+        Movie("Vampire's Kiss", 1988),
+        Movie("Con Air", 1997),
+        Movie("Gone in 60 Seconds", 1997),
+        Movie("National Treasure", 2004),
+        Movie("The Wicker Man", 2006),
+        Movie("Ghost Rider", 2007),
+        Movie("Knowing", 2009)
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +35,11 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // View要素を見つけて、ここで何かをする
+        // RecyclerViewを初期化する
+        list_recycler_view.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = ListAdapter(mNicolasCageMovies)
+        }
     }
 
     companion object {
